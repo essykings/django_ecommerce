50547e47cddea79e83fb439c531c8c41a02032c8
vaatiesther <essyking@gmail.com>
2023-08-23 23:37:26 +0300

    Update settings.py

--- lux/settings.py
+++ lux/settings.py
@@ -107,9 +107,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATICFILES_DIRS = [BASE_DIR / 'static']  
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
 
 import os
 MEDIA_URL ='/media/'
